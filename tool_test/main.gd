dd9ff114da02c973013f5e9f47b1c350929d03ea
guandong <1623088350@qq.com>
2023-02-24 09:26:30 +0800

    Update main.gd

--- tool_test/main.gd
+++ tool_test/main.gd
@@ -4,8 +4,10 @@ extends Control
 func _run():
 	_ready()
 	
+var screen_size : Vector2 = Vector2()
 
-
+func _on_root_resize():
+	rect_size =  get_tree().root.size  # get_node("/root")
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -19,8 +21,31 @@ func _ready():
 	else:
 		print('code exec in real scene.')
 	pass # Replace with function body.
+	rect_size =  get_tree().root.size  # get_node("/root")
+	print(rect_size)
+	get_tree().root.connect("size_changed", self, "myfunc")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta):
+	if rect_size != get_tree().root.size:
+		rect_size = get_tree().root.size
+	
+		
+
+func myfunc():
+	print("Resizing: ", get_viewport_rect().size, OS.get_window_size())
+	self.set_size( OS.get_window_size() )
+
+"""
+
+
+func _ready():
+screen_size = OS.get_screen_size()# Gets the screen size to test in futur if its change since their
+
+func _process(delta) -> void:
+	if OS.get_screen_size() != screen_size: #Tests if your screen changed in size, e.g a different monitor
+		screen_size = OS.get_screen_size()
+		OS.set_window_size(screen_size)# Sets your window to your screen size
+"""
