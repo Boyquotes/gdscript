a39bcf6195567b7e2011aba77f214ae6934e7ae0
guandong <1623088350@qq.com>
2023-02-27 17:25:15 +0800

    gd4.0 godello clone

---
+++ gd4.0_godello/scene/main_scene.gd
@@ -0,0 +1,12 @@
+extends ColorRect
+@onready var content_container := $ContentContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#DisplayServer.window_set_size(Vector2i(1024, 600))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
