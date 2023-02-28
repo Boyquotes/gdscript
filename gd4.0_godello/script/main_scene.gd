39e061ed0bc4ab494513860561e955aa9d74ca47
guandong <1623088350@qq.com>
2023-02-28 10:02:51 +0800

    add popup window

--- gd4.0_godello/script/main_scene.gd
+++ gd4.0_godello/script/main_scene.gd
@@ -4,6 +4,7 @@ extends ColorRect
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#DisplayServer.window_set_size(Vector2i(1024, 600))
+	SceneUtils.create_single_error_popup("test pop window.", self, self)
 	pass # Replace with function body.
 
 
