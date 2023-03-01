e0e43412f645234599caa1f50229b0cbe3fb5dae
guandong <1623088350@qq.com>
2023-03-01 16:28:12 +0800

    add ContentContainer

--- gd4.0_godello/script/main_scene.gd
+++ gd4.0_godello/script/main_scene.gd
@@ -1,10 +1,16 @@
 extends ColorRect
 
+@onready var content_container := $ContentContainer
+
+var boards
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#DisplayServer.window_set_size(Vector2i(1024, 600))
-	SceneUtils.create_single_error_popup("test pop window......", self, self)
-	pass # Replace with function body.
+	boards = load("res://scenes/boards.tscn").instantiate()
+	content_container.add_child(boards)
+	#SceneUtils.create_single_error_popup("test pop window......", self, self)
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
