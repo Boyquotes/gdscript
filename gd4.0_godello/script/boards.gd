210866f9fdd87bf6ea959664541711f6c7ab9dd2
guandong <1623088350@qq.com>
2023-03-03 15:46:03 +0800

    add brds scene

--- gd4.0_godello/script/boards.gd
+++ gd4.0_godello/script/boards.gd
@@ -1,12 +1,16 @@
 extends ColorRect
 
-@onready var personal_boards_container := $ScrollContainer/MarginContainer/BoxContainer/PersonalBoardsContainer
-
+@onready var personal_boards_container := $MarginContainer/BoxContainer/PersonalBoardsContainer
+@onready var create_Button := $MarginContainer/BoxContainer/PersonalBoardsContainer/Button
 func _on_CreateBoard_pressed():
 	DataRepository.create_board()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for i in range(1):
+		var btn_clone = create_Button.duplicate()
+		btn_clone.set_text(str(i))
+		personal_boards_container.add_child(btn_clone)
 	pass # Replace with function body.
 
 
