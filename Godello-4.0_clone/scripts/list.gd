cbd87b913b7cbe467f7ae10eea6e7eed41ae3271
guandong <1623088350@qq.com>
2023-04-12 17:42:51 +0800

    add card_preview

--- Godello-4.0_clone/scripts/list.gd
+++ Godello-4.0_clone/scripts/list.gd
@@ -2,15 +2,16 @@ extends PanelContainer
 
 @onready var card_container := $MarginContainer/VerticalContent/CardContainerScroll/CardContainer
 const CardScene := preload("res://scenes/card.tscn")
-
+const cardPreview := preload("res://scenes/card_mouse_preview.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(3):
 		var card_element = CardScene.instantiate()
 		card_container.add_child(card_element)
 	
-
+func _get_drag_data(_pos):
+	return { "cardID":"001" }
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
