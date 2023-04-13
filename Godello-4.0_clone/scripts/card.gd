ea07b0f18105e18da834fc6d313e956b90bb162d
guandong <1623088350@qq.com>
2023-04-13 15:59:03 +0800

    实现列表元素拖拽 done.

--- Godello-4.0_clone/scripts/card.gd
+++ Godello-4.0_clone/scripts/card.gd
@@ -2,6 +2,8 @@ extends MarginContainer
 
 @onready var title_label := $CardContent/InnerPadding/VBoxContainer/TitleContainer/Title
 
+signal card_hover_changed(card_hover)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,5 +14,9 @@ func setTitle(title):
 func getTitle():
 	return title_label.get_text()
 
+func _on_mouse_entered(): # 鼠标第一次移到控件上
+	mouse_default_cursor_shape = 1
+	card_hover_changed.emit(self)
 
-
+func _on_mouse_exited():  # 鼠标离开控件
+	card_hover_changed.emit(null)
--- Godello-4.0_clone/scripts/list.gd
+++ Godello-4.0_clone/scripts/list.gd
@@ -4,41 +4,43 @@ extends PanelContainer
 @onready var card_container := $MarginContainer/VerticalContent/CardContainerScroll/CardContainer
 const CardScene := preload("res://scenes/card.tscn")
 const cardPreview := preload("res://scenes/card_mouse_preview.tscn")
+
+var card_hover = null
+var card_draged = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(3):
 		var card_element = CardScene.instantiate()
 		card_container.add_child(card_element)
 		card_element.setTitle(str(i))
+		
+		card_element.card_hover_changed.connect(on_card_hover_changed)
+		
+func on_card_hover_changed(card):
+	card_hover = card
 
 func _get_drag_data(pos):
 	var preview = cardPreview.instantiate()
 	set_drag_preview(preview)
-	
-	var closest_child
-	var last_distance : float = -1
-	var is_before := true
-
-	var scrolled_mouse_pos := Vector2(pos.x, pos.y + card_container_scroll.get_v_scroll())
-
-	for child in card_container.get_children():
-		var distance : float = child.get_position().distance_to(scrolled_mouse_pos)
-
-		if last_distance == -1 or (distance < last_distance):
-			last_distance = distance
-			closest_child = child
-	
-	var title = closest_child.getTitle()
-	print("closest_child: " + title)
-	
-	#return { "cardID":"001" }
-#	print(closest_child.)
-	return closest_child
+	card_draged = card_hover
+	return card_draged
 
 func _can_drop_data(pos, card_draged):
-	return true
+	var allow = true if card_hover != null else false
+	return allow
 
 func _drop_data(pos, card_draged):
+	
+	if card_draged == null:
+		return
+	
+	if card_hover == null:
+		return
+
+	if card_hover == card_draged:
+		return
+#	
 	var closest_child
 	var last_distance : float = -1
 	var is_before := true
@@ -52,30 +54,12 @@ func _drop_data(pos, card_draged):
 			last_distance = distance
 			closest_child = child
 	
+	var index = -1
+	for child in card_container.get_children():
+		index += 1
+		if child == card_hover:
+			break
+	
+	if index != -1:	
+		card_container.move_child(card_draged, index)
 
-func find_closest_vertical_child(mouse_pos, compare_to, container, container_scroll) -> Array:
-	var closest_child
-	var last_distance : float = -1
-	var is_before := true
-
-	var scrolled_mouse_pos := Vector2(mouse_pos.x, mouse_pos.y + container_scroll.get_v_scroll())
-
-	for child in container.get_children():
-		var distance : float = child.get_position().distance_to(scrolled_mouse_pos)
-
-		if last_distance == -1 or (distance < last_distance):
-			last_distance = distance
-			closest_child = child
-
-	if closest_child and closest_child != compare_to:
-		var y = closest_child.get_position().y
-		var height = closest_child.get_size().y
-		is_before = scrolled_mouse_pos.y <= (y + height * 0.5)
-		return [closest_child, is_before]
-
-	return [false]
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
